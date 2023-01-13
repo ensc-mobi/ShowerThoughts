288c7e6353cbd81f3e717adb43cc38d999998b0c
Baptiste Pesquet <bpesquet@gmail.com>
2023-01-13 11:03:39 +0100

    Add local storage

--- App.js
+++ App.js
@@ -1,11 +1,49 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, View, TextInput, FlatList } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const STORAGE_KEY = "ASYNC_STORAGE_IDEAS";
 
 export default function App() {
   // Value of the text input
   const [input, setInput] = useState("");
   // Ideas list, initially empty
   const [ideas, setIdeas] = useState([]);
+
+  const saveIdeas = async () => {
+    try {
+      // Turn ideas array into a JSON string
+      const jsonIdeas = JSON.stringify(ideas);
+      // Store ideas string
+      await AsyncStorage.setItem(STORAGE_KEY, jsonIdeas);
+    } catch (e) {
+      console.error("Failed to save ideas");
+    }
+  };
+
+  const loadIdeas = async () => {
+    // Load ideas from local storage
+    try {
+      // Load ideas string
+      const jsonIdeas = await AsyncStorage.getItem(STORAGE_KEY);
+      if (jsonIdeas !== null) {
+        // Turn stored JSON string into an array, and set it as ideas array
+        setIdeas(JSON.parse(jsonIdeas));
+      }
+    } catch (e) {
+      console.error("Failed to load ideas");
+    }
+  };
+
+  // Load ideas only during initial component mounting
+  useEffect(() => {
+    loadIdeas();
+  }, []);
+
+  // Save ideas whenever its content changes
+  useEffect(() => {
+    saveIdeas();
+  }, [ideas]);
 
   return (
     <View style={styles.container}>
@@ -19,7 +57,9 @@ export default function App() {
           setInput(text);
         }}
         onSubmitEditing={() => {
-          // Append new idea (input value) at end of array
+          if (!input) return; // Don't submit if empty
+
+          // Append new idea at end of ideas array
           setIdeas((ideas) => [...ideas, input]);
           // Reset input value
           setInput("");
