c2774798dbd592c382d4dfc54c26eba72c8e2810
Baptiste Pesquet <bpesquet@gmail.com>
2023-01-13 09:55:40 +0100

    Stateless version

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, FlatList } from "react-native";
 
 export default function App() {
+  // Value of the text input
+  const [input, setInput] = useState("");
+  // Ideas list, initially empty
+  const [ideas, setIdeas] = useState([]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        style={styles.input}
+        placeholder="Enter your newest brilliant idea"
+        // Display input value
+        value={input}
+        onChangeText={(text) => {
+          // Update input value
+          setInput(text);
+        }}
+        onSubmitEditing={() => {
+          // Append new idea (input value) at end of array
+          setIdeas((ideas) => [...ideas, input]);
+          // Reset input value
+          setInput("");
+        }}
+      />
+      <FlatList
+        style={styles.list}
+        data={ideas}
+        renderItem={({ item }) => <Text style={styles.item}>- {item}</Text>}
+        keyExtractor={(item) => item}
+      ></FlatList>
     </View>
   );
 }
@@ -13,8 +38,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 30,
+  },
+  input: {
+    backgroundColor: "whitesmoke",
+    padding: 10,
+    marginBottom: 10,
+  },
+  list: {
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  item: {
+    padding: 5,
+    //fontSize: 16,
   },
 });
